d19cbe53f1cd09ea7afe259f66afbf17ff1f8fff
Александр Середин <agames1448@gmail.com>
2022-09-28 20:55:29 +0300

    Добавьте файлы проекта.

---
+++ FundamentalsOfDesignPatterns/Program.cs
@@ -0,0 +1,26 @@
+﻿using FundamentalsOfDesignPatterns;
+
+Console.WriteLine("Hello, World!");
+
+
+Test1 tesst1 = new Test2();
+tesst1.Print1();
+
+Test2 test2 = (Test2)tesst1;
+test2.Print2();
+
+Test3? test3 = tesst1 as Test3;
+test3?.Print3();
+
+if (test3 is Test2)
+{
+    test3.Print2();
+}
+else
+{
+    Console.WriteLine("НЕ приводиться");
+}
+
+Person tom = new Employee("Tom", "Microsoft");
+
+tom.Print();
---
+++ FundamentalsOfDesignPatterns/Test.cs
@@ -0,0 +1,66 @@
+﻿namespace FundamentalsOfDesignPatterns
+{
+    internal class Test1
+    {
+        public  const string test1 = " Test1";
+        public void Print1()
+        {
+            Console.WriteLine($"{test1}");
+        }
+    }
+
+    internal class Test2 : Test1
+    {
+        public const string test2 = " Test2";
+        public void Print2()
+        {
+            Console.WriteLine($"{test1},{test2}");
+        }
+    }
+
+    internal class Test3 : Test2
+    {
+        public const string test3 = " Test1";
+        public void Print3()
+        {
+            Console.WriteLine($"{test1},{test2},{test3}");
+        }
+    }
+
+    internal class Test4 : Test3
+    {
+        public const string test4 = " Test1";
+
+        public void Print4()
+        {
+            Console.WriteLine($"{test1},{test2},{test3},{test4},");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string name, string company)
+            : base(name)
+        {
+            Company = company;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"{Name} работает в {Company}");
+        }
+    }
+}
